3e8c41f2b2e637ce7ba93ca43e5e172a1af407c6
Greg <gregory.marshall999@gmail.com>
2024-08-26 12:26:32 +0200

    rendu déclaratif

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+
+<h2>{{ message }}</h2>
+<p>Le compte: {{ compteur.compte + 1 }}</p>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      message: 'Coucou', 
+      compteur: {
+        compte: 0
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
