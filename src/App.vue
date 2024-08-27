64af47e9f8f2c960e69a10b9f87f865ebe7fd793
Greg <gregory.marshall999@gmail.com>
2024-08-27 11:10:34 +0200

    slots

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,30 @@
     @envoyerTitre="titreEnvoye"
     :titre="passerAEnfant02.titre" 
     :textes="passerAEnfant02.textes"/>
+
+  <AffichageSimple>
+    J'ai initialisé le composant. Pour un slot simple.
+  </AffichageSimple>
+
+  <AffichageComplexe>
+    <template v-slot:header>
+      Titre du Lorem
+    </template>
+
+    <template #default>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida porttitor porttitor. Mauris dictum turpis ac velit feugiat, non vulputate neque tempor. Pellentesque in imperdiet metus. Aliquam erat volutpat. Pellentesque quis justo magna. Praesent sit amet elit neque. Etiam accumsan commodo nisl, in semper nibh feugiat non. Vestibulum felis lacus, pulvinar eu tellus ac, tincidunt egestas libero. Mauris iaculis sem elit, ac fermentum sem vulputate at. 
+    </template>
+    
+    <template #footer>
+      <p>Le footer du Lorem</p>
+    </template>
+  </AffichageComplexe>
 </template>
 
 <script setup>
 import Affichage from './components/Affichage.vue';
+import AffichageSimple from './components/AffichageSimple.vue';
+import AffichageComplexe from './components/AffichageComplexe.vue';
 import { ref } from 'vue';
 
 const passerAEnfant01 = ref({
